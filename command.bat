7b1f9d24229e78f2cdba983b175639896ec2523b
shiroi36 <keita104824@gmail.com>
2023-07-31 12:06:06 +0900

    2023年7月31日製作要領書追加

--- command.bat
+++ command.bat
@@ -1,7 +1,7 @@
 git init
 git remote add origin git@github.com:shiroi36/00WCSTemplate.git
 git add .
-git commit -m 2023”N4Œ20“ú‰c‹Æ‘—¿’Ç‰Á
+git commit -m 2023”N7Œ31“ú»ì—v—Ì‘’Ç‰Á
 git push origin master
 pause
 exit
