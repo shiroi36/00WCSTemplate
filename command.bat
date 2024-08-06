4dc82d616a7b69b8cf55b02c88444c8d3cd8789f
keita_cf-sv8 <keita104824@gmail.com>
2024-08-06 10:47:31 +0900

    2024�N8��6��WCS���������̍X�V

--- command.bat
+++ command.bat
@@ -2,7 +2,7 @@ chcp 65001
 git init
 git remote add origin git@github.com:shiroi36/00WCSTemplate.git
 git add .
-git commit -m 2024年4月3日検査シートマクロの更新
+git commit -m 2024年8月6日WCS建方資料の更新
 git push origin main
 pause
 exit
